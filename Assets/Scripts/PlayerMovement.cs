a46627dcd4ceb3676aa6fff23da6fd9d94bd4a5a
Ash Morrissey <a.morrisseyy@gmail.com>
2022-04-09 08:55:18 +0100

    PlayerMovement.cs edited to allow player to run left and right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 1f;
     Vector2 moveInput;
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove (InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x*runSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
     }
 }

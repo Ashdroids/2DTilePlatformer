31cb22b678697036603048c4324195788ab2c325
Ash Morrissey <a.morrisseyy@gmail.com>
2022-04-09 13:43:27 +0100

    PlayerMMovement.cs edited to change to running animation OnRun

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,21 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 1f;
+    Animator animator;
     Vector2 moveInput;
     Rigidbody2D rb;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove (InputValue value)
@@ -28,5 +32,19 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x*runSpeed, rb.velocity.y);
         rb.velocity = playerVelocity;
+
+        bool playerHasHorizontalMovement = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        animator.SetBool("isRunning", playerHasHorizontalMovement);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalMovement = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalMovement)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(rb.velocity.x) , 1f);
+        }
     }
 }
